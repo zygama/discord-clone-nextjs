e5818eae586998239cfed4565071bf6e65445719
Sofiane Lamrani <sofiane.lamrani@webedia-group.com>
2023-10-31 00:34:51 +0100

    Messages API

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -41,6 +42,16 @@ const ChannelIdPage: React.FC<ChannelIdPageProps> = async ({ params }) => {
         name={channel.name}
         serverId={params.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future message</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
