9d2e2e65852e73d921b7e313ef3c0e5c582c1fb0
Sofiane Lamrani <sofiane.lamrani@webedia-group.com>
2023-10-31 00:46:03 +0100

    Message attachment

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -11,12 +11,15 @@ export type ModalType =
   | "leaveServer"
   | "deleteServer"
   | "deleteChannel"
-  | "editChannel";
+  | "editChannel"
+  | "messageFile";
 
 interface ModalData {
   server?: Server;
   channelType?: ChannelType;
   channel?: Channel;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
 interface ModalStore {
