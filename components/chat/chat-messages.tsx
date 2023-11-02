12f0cb61fcbf55e1399a1b22b35a937f3b3aa91b
Sofiane Lamrani <sofiane.lamrani@webedia-group.com>
2023-11-02 02:57:31 +0100

    Chat item component + delete message modal

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -3,10 +3,12 @@
 import { Fragment } from "react";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
+import { format } from "date-fns";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import ChatWelcome from "./chat-welcome";
+import { ChatItem } from "./chat-item";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -25,6 +27,8 @@ interface ChatMessagesProps {
   paramValue: string;
   type: "channel" | "conversation";
 }
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 export const ChatMessages = ({
   name,
@@ -77,7 +81,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>{message.content}</div>
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
